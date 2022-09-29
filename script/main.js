e4a93af8f58bcd2d92e40764f7a3853b969ae042
Andrea Capolupo <lupoandrea98@hotmail.com>
2022-09-29 19:43:30 +0200

    Initialize

---
+++ script/main.js
@@ -0,0 +1,46 @@
+window.addEventListener('load', function(){
+    //canvas setup
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 1500;
+    canvas.height = 500;
+
+    class inputHandler{
+
+    }
+
+    class ProjectTitle {
+
+    }
+
+    class Particle {
+
+    }
+
+    class Player {
+
+    }
+
+    class Enemy {
+
+    }
+
+    class Layer {
+
+    }
+
+    class Background {
+
+    }
+
+    class UI {
+
+    }
+
+    class Game {
+        
+    }
+
+
+
+});
