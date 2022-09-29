1cd4d0976bcecffa85207e2e7ff166b830e3dd56
Andrea Capolupo <lupoandrea98@hotmail.com>
2022-09-29 20:50:36 +0200

    Adding shooting properties

--- script/main.js
+++ script/main.js
@@ -12,6 +12,8 @@ window.addEventListener('load', function(){
                 console.log(this.game.keys.indexOf(e.key));
                 if (((e.key === 'ArrowUp') || (e.key === 'ArrowDown')) && (this.game.keys.indexOf(e.key) === -1)) {
                     this.game.keys.push(e.key);
+                }else if(e.key === ' ') {
+                    this.game.player.shootTop();
                 }
                 console.log(this.game.keys);
             });
@@ -25,6 +27,26 @@ window.addEventListener('load', function(){
     }
 
     class Projectile {
+        constructor(game, x, y) {
+            this.game = game;
+            this.x = x;
+            this.y = y;
+            this.width = 10;
+            this.height = 3;
+            this.speed = 3;
+            this.markedForDeletion = false;
+        }
+
+        update() {
+            this.x += this.speed;
+            if(this.x > this.game.width * 0.8) 
+                this.markedForDeletion = true;
+        }
+
+        draw(context) {
+            context.fillStyle = 'yellow';
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
 
     }
 
@@ -41,15 +63,29 @@ window.addEventListener('load', function(){
             this.y = 100;
             this.speedY = 0;
             this.maxSpeed = 2;
+            this.projectiles = [];
         }
         update() {
             if(this.game.keys.includes('ArrowUp')) this.speedY = -this.maxSpeed;
             else if(this.game.keys.includes('ArrowDown')) this.speedY = this.maxSpeed;
             else this.speedY = 0
             this.y += this.speedY;
+
+            //handle projectiles
+            this.projectiles.forEach(projectiles => {
+                projectiles.update();
+            });
+            this.projectiles = this.projectiles.filter(projectile => !projectile.markedForDeletion);
         }
         draw(context) {
+            context.fillStyle = 'black';
             context.fillRect(this.x, this.y, this.width, this.height);
+            this.projectiles.forEach(projectiles => {
+                projectiles.draw(context);
+            });
+        }
+        shootTop() {
+            this.projectiles.push(new Projectile(this.game, this.x + 120, this.y))
         }
     }
 
